321224e79dbc2bf853336ed4266b28c3a23f820c
DarjaElina <daria.elina98@gmail.com>
2025-12-30 20:43:51 +0200

    add 'is' keyword basics

---
+++ basics_2/is_vs_equal.py
@@ -0,0 +1,23 @@
+print(True == 1) # True
+print('' == 1) # False
+print([] == 1) # False
+print(10 == 10.0) # True
+print([] == []) # True
+
+print("🦋 ================================ 🦋")
+
+print(True is 1) # False
+print('' is 1) # False
+print([] is 1) # False
+print(10 is 10.0) # False
+print([] is []) # False
+print(1 is 1) # 
+
+hi = "hi"
+hi_2 = "hi"
+
+print(hi is hi_2) # True
+
+x = 1
+y = 1
+print(x is y) # True
