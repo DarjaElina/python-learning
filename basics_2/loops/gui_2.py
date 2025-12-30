1507035492fc1006428c16c4d132289bc6cbf5e7
DarjaElina <daria.elina98@gmail.com>
2025-12-30 22:43:59 +0200

    add gui exercise simple version

---
+++ basics_2/loops/gui_2.py
@@ -0,0 +1,18 @@
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+
+symbols = {
+  0: ' ',
+  1: '*'
+}
+
+for row in picture:
+  for pixel in row:
+    print(symbols[pixel], end="")
+  print()
